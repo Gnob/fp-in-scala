25e47dcbd209fc2d291de962a025f945ae29d76e
Gnob <gnob244@gmail.com>
2017-03-02 01:31:59 +0900

    [Chapter3] ing

--- src/main/scala/fpinscala/datastructures/List.scala
+++ src/main/scala/fpinscala/datastructures/List.scala
@@ -41,10 +41,114 @@ object List {
       else drop(xs, n - 1)
   }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+  def dropWhile1[A](l: List[A], f: A => Boolean): List[A] = l match {
     case Nil => Nil
     case Cons(x, xs) =>
-      if (f(x)) dropWhile(xs, f)
+      if (f(x)) dropWhile1(xs, f)
       else l
   }
+
+  // currying을 통한 형식 추론
+  def dropWhile2[A](l: List[A])(f: A => Boolean): List[A] = l match {
+    case Cons(x, xs) if f(x) => dropWhile2(xs)(f)
+    case _ => l
+  }
+
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(h, hs) => Cons(h, init(hs))
+  }
+
+  /*
+  foldRight(Cons(1, Cons(2, Cons(3, Cons(4, Nil)))), 0)(_ + _)
+  = 1 + foldRight(Cons(2, Cons(3, Cons(4, Nil))), 0)(_ + _)
+  = 1 + (2 + foldRight(Cons(3, Cons(4, Nil)), 0)(_ + _))
+  = 1 + (2 + (3 + foldRight(Cons(4, Nil), 0)(_ + _)))
+  = 1 + (2 + (3 + (4 + foldRight(Nil, 0)(_ + _))))
+  = 1 + (2 + (3 + (4 + 0)))
+
+  foldRight(Cons(1, Cons(2, Cons(3, Cons(4, Nil)))), Nil: List[Int])((x, y) => Cons(y, x)
+  = Cons(1, foldRight(Cons(2, Cons(3, Cons(4, Nil))), 0)(_ + _))
+  = Cons(1, Cons(2, foldRight(Cons(3, Cons(4, Nil)), 0)(_ + _)))
+  = Cons(1, Cons(2, Cons(3, foldRight(Cons(4, Nil), 0)(_ + _))))
+  = Cons(1, Cons(2, Cons(3, Cons(4, foldRight(Nil, 0)(_ + _)))))
+  = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+   */
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  def sumByFoldRight(ns: List[Int]) = foldRight(ns, 0)((x, y) => x + y)
+
+  def productByFoldRight(ns: List[Double]) = foldRight(ns, 1.0)(_ * _)
+
+  def lengthByFoldRight[A](l: List[A]) = foldRight(l, 0)((_, y) => 1 + y)
+
+  def makeList1[A](v: A, n: Int): List[A] = {
+    def loop(n: Int): List[A] =
+      if (n == 0) Nil
+      else Cons(v, loop(n - 1))
+
+    loop(n)
+  }
+
+  def append[A](l: List[A], v: List[A]): List[A] = l match {
+    case Nil => v
+    case Cons(h, tail) => Cons(h, append(tail, v))
+  }
+
+  def makeList2[A](v: A, n: Int): List[A] = {
+    var a:List[A] = Nil
+    for (_ <- 1 to n)
+      a = Cons(v, a)
+    a
+  }
+
+  /* This is wrong foldLeft!
+  foldLeft(Cons(1, Cons(2, Cons(3, Cons(4, Nil)))), 0)(_ + _)
+  = foldLeft(Cons(2, Cons(3, Cons(4, Nil))), 0)(_ + _) + 1
+  = (foldLeft(Cons(3, Cons(4, Nil)), 0)(_ + _) + 2) + 1
+  = ((foldLeft(Cons(4, Nil), 0)(_ + _) + 3) + 2) + 1
+  = (((0 + 4) + 3) + 2) + 1
+
+  foldLeft(Cons(1, Cons(2, Cons(3, Cons(4, Nil)))), Nil: List[Int])((x, y) => Cons(y, x)
+  = Cons(1, foldLeft(Cons(2, Cons(3, Cons(4, Nil))), Nil: List[Int])((x, y) => Cons(y, x))
+  = Cons(1, Cons(2, foldLeft(Cons(3, Cons(4, Nil)), Nil: List[Int])((x, y) => Cons(y, x)))
+  = Cons(1, Cons(2, Cons(3, foldLeft(Cons(4, Nil), Nil: List[Int])((x, y) => Cons(y, x))))
+  = Cons(1, Cons(2, Cons(3, Cons(4, foldLeft(Nil, Nil: List[Int])((x, y) => Cons(y, x)))))
+  = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+   */
+  def wrongFoldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil => z
+    case Cons(h, tail) => f(wrongFoldLeft(tail, z)(f), h)
+  }
+
+  /*
+  foldLeft(Cons(1, Cons(2, Cons(3, Cons(4, Nil)))), 0)(_ + _)
+  = foldLeft(Cons(2, Cons(3, Cons(4, Nil))), 0 + 1)(_ + _)
+  = foldLeft(Cons(3, Cons(4, Nil)), (0 + 1) + 2)(_ + _)
+  = foldLeft(Cons(4, Nil), ((0 + 1) + 2) + 3)(_ + _)
+  = (((0 + 1) + 2) + 3) + 4
+
+  foldLeft(Cons(1, Cons(2, Cons(3, Cons(4, Nil)))), Nil: List[Int])((x, y) => Cons(y, x)
+  = foldLeft(Cons(2, Cons(3, Cons(4, Nil))), Cons(1, Nil))((x, y) => Cons(y, x))
+  = foldLeft(Cons(3, Cons(4, Nil)), Cons(2, Cons(1, Nil)))((x, y) => Cons(y, x))
+  = foldLeft(Cons(4, Nil), Cons(3, Cons(2, Cons(1, Nil))))((x, y) => Cons(y, x))
+  = foldLeft(Nil, Cons(4, Cons(3, Cons(2, Cons(1, Nil)))))((x, y) => Cons(y, x))
+  = Cons(4, Cons(3, Cons(2, Cons(1, Nil))))
+   */
+  def foldLeft[A,B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil => z
+    case Cons(h, tail) => foldLeft(tail, f(z, h))(f)
+  }
+
+  def sumByFoldLeft(ns: List[Int]) = foldLeft(ns, 0)((x, y) => y + x)
+
+  def productByFoldLeft(ns: List[Double]) = foldLeft(ns, 1.0)(_ * _)
+
+  def lengthByFoldLeft[A](l: List[A]) = foldLeft(l, 0)((x, _) => x + 1)
+
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, Nil: List[A])((x, y) => Cons(y, x))
 }
--- src/test/scala/fpinscala/datastructures/ListSpec.scala
+++ src/test/scala/fpinscala/datastructures/ListSpec.scala
@@ -12,7 +12,7 @@ class ListSpec extends UnitSpec {
     assertCompiles("val ex3: List[String] = Cons(\"a\", Cons(\"b\", Nil))")
   }
 
-  "List" should "be existed" in {
+  it should "be existed" in {
     val list = Cons(1, Nil)
     assert(List.tail(list) == Nil)
     assert(List.setHead(list, 2) == Cons(2, Nil))
@@ -25,7 +25,7 @@ class ListSpec extends UnitSpec {
     assert(List.setHead(longList, 2) == List(2, 2, 3, 4))
   }
 
-  "List" should "be dropped" in {
+  it should "be dropped" in {
     val longList = List(1, 2, 3, 4)
     assert(List.drop(longList, 1) == List(2, 3, 4))
     assert(List.drop(longList, 1) == List.tail(longList))
@@ -34,8 +34,58 @@ class ListSpec extends UnitSpec {
     assert(List.drop(longList, 4) == Nil)
     assert(List.drop(longList, 5) == Nil)
 
-    assert(List.dropWhile(longList, (x: Int) => x < 1) == longList)
-    assert(List.dropWhile(longList, (x: Int) => x < 3) == List(3, 4))
-    assert(List.dropWhile(longList, (x: Int) => x < 5) == Nil)
+    assert(List.dropWhile1(longList, (x: Int) => x < 1) == longList)
+    assert(List.dropWhile1(longList, (x: Int) => x < 3) == List(3, 4))
+    assert(List.dropWhile1(longList, (x: Int) => x < 5) == Nil)
+
+    assert(List.dropWhile2(longList)(x => x < 4) == List(4))
+  }
+
+  it should "append and pop" in {
+    val longList = List(1, 2, 3, 4)
+    val shortList = List(5, 6)
+
+    assert(List.init(longList) == List(1, 2, 3))
+    assert(List.append(longList, shortList) == List(1, 2, 3, 4, 5 ,6))
+  }
+
+  it should "be folded right" in {
+    val longList = List(1, 2, 3, 4)
+    val doubleList = List(1.0, 2.0, 3.0, 4.0)
+    assert(List.foldRight(longList, 0)(_ + _) == List.sum(longList))
+
+    assert(List.sumByFoldRight(longList) == List.sum(longList))
+    assert(List.productByFoldRight(doubleList) == List.product(doubleList))
+
+    assert(List(1, 2, 3, 4) == List.foldRight(longList, Nil: List[Int])(Cons(_, _)))
+
+    assert(List.lengthByFoldRight(doubleList) == 4)
+
+    val loooongList = List.makeList2(1, 10000)
+    assertThrows[StackOverflowError] { List.foldRight(loooongList, 0)(_ + _) }
+
+    assert(List.foldLeft(longList, 0)(_ + _) == List.sum(longList))
+  }
+
+  it should "be folded left" in {
+    val longList = List(1, 2, 3, 4)
+    val doubleList = List(1.0, 2.0, 3.0, 4.0)
+    assert(List.foldLeft(longList, 0)(_ + _) == List.sum(longList))
+    assert(List.sumByFoldLeft(longList) == List.sum(longList))
+    assert(List.productByFoldLeft(doubleList) == List.product(doubleList))
+
+    assert(List(4, 3, 2, 1) == List.foldLeft(longList, Nil: List[Int])((x, y) => Cons(y, x)))
+
+    assert(List.lengthByFoldLeft(doubleList) == 4)
+
+    val loooongList = List.makeList2(1, 10000)
+    assertResult(10000) { List.foldLeft(loooongList, 0)(_ + _) }
+  }
+
+  "FoldRight and FoldLeft" should "be equal" in {
+    val testList = List("A", "B", "C", "D")
+
+    assert(List.foldRight(testList, "")(_ + _) == List.foldLeft(testList, "")(_ + _))
+    assert(List.foldRight(testList, "")(_ + _) != List.foldLeft(testList, "")((x, y) => y + x))
   }
 }
