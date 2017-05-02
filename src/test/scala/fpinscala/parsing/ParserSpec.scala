886d5323281c3ee237305c95dc3ebd0ca7c0512b
Gnob <gnob244@gmail.com>
2017-05-02 10:28:56 +0900

    [Chapter 9] ing

---
+++ src/test/scala/fpinscala/parsing/ParserSpec.scala
@@ -0,0 +1,11 @@
+package fpinscala.parsing
+
+import fpinscala.test.UnitSpec
+
+/**
+  * Created by Gnob on 2017. 4. 26..
+  */
+class ParserSpec extends UnitSpec {
+  val c: Char = 'c'
+  run(char(c))(c.toString) == Right(c)
+}
